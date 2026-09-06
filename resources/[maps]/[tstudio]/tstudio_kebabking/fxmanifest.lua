626dda3b332a2b2f9571dec419120b2c3401a634
CrazyCaseyy <106638945+CrazyCaseyy@users.noreply.github.com>
2026-09-06 15:18:36 -0700

    map

---
+++ resources/[maps]/[tstudio]/tstudio_kebabking/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+lua54 'yes'
+game "gta5"
+
+author 'tstudio - Adr0o'
+description 'Kebab King Diner Store Legion Square'
+version '1.1.1'
+
+this_is_a_map "yes"
+
+dependencies { 
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    '/gameBuild:2545',  -- ⚠️PLEASE READ⚠️; Requires at least GAME build 2545.
+    'tstudio_zmapdata',  -- ⚠️PLEASE READ⚠️; Requires to be started before this resource.
+}
+
+escrow_ignore {
+    'stream/vanilla/*.*',
+    'stream/custom/ytd/*.ytd',
+}
+dependency '/assetpacks'
