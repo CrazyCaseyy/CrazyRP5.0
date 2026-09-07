e55897cadc6f56a244493111a078a2b292bb1ed7
CrazyCaseyy <106638945+CrazyCaseyy@users.noreply.github.com>
2026-09-07 13:27:03 -0700

    doorlock

--- resources/[ox]/ox_doorlock/config.lua
+++ resources/[ox]/ox_doorlock/config.lua
@@ -6,16 +6,15 @@ Config = {}
 Config.Notify = false
 
 ---Create a persistent notification while in-range of a door, prompting to lock/unlock.
-Config.DrawTextUI = false
+-- The "[E] Lock/Unlock door" ox_lib text UI prompt (locales/en.json already
+-- has the "[E]" baked into the label) - same lib.showTextUI style used
+-- everywhere else in this project instead of a floating world sprite.
+Config.DrawTextUI = true
 
 ---Set the properties used by [DrawSprite](https://docs.fivem.net/natives/?_0xE7FFAE5EBF23D890).
-Config.DrawSprite = {
-    -- Unlocked
-    [0] = { 'mpsafecracking', 'lock_open', 0, 0, 0.018, 0.018, 0, 255, 255, 255, 100 },
-
-    -- Locked
-    [1] = { 'mpsafecracking', 'lock_closed', 0, 0, 0.018, 0.018, 0, 255, 255, 255, 100 },
-}
+-- Disabled - the lock icon hovering over every door is replaced by the
+-- DrawTextUI prompt above.
+Config.DrawSprite = false
 
 ---Allow the specified ace principal to use 'command.doorlock'.
 Config.CommandPrincipal = 'group.admin'
